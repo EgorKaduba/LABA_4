d00a1e1ea416760f726fa593f6cb777060803103
egork <egor.kaduba@mail.ru>
2024-12-12 23:47:59 +0300

    Добавил кнопку меню. Контакты, помощь

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from aiogram import Bot, Dispatcher, F
 from aiogram.filters import Command
-from aiogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
+from aiogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery, BotCommand
 
 from config import load_config
 
@@ -23,5 +23,28 @@ async def start(message: Message):
                          reply_markup=inline_keyboar)
 
 
+@dp.message(Command(commands='contacts'))
+async def contacts(message: Message):
+    await message.answer("Электронная почта: egor.kaduba@mail.ru\nТелеграмм: @sTeNki_ok")
+
+
+@dp.message(Command(commands='help'))
+async def bot_help(message: Message):
+    await message.answer("Пока тут ничего нет, но скоро появится")
+
+
+async def set_main_menu():
+    # Создаем список с командами и их описанием для кнопки menu
+    main_menu_commands = [
+        BotCommand(command='/help',
+                   description='Справка по работе бота'),
+        BotCommand(command='/contacts',
+                   description='Другие способы связи'),
+    ]
+
+    await bot.set_my_commands(main_menu_commands)
+
+
+dp.startup.register(set_main_menu)
 if __name__ == "__main__":
     dp.run_polling(bot)
